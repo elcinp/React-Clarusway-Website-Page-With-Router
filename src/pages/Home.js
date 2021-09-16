0e60ec4a41efbb4c12ac2b0635a95b002c0523b7
elcinp <elcinpars1@gmail.com>
2021-09-16 14:52:18 +0300

    html,css,brush images imported

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+import html from '../assets/img/logo_html.png'
+import css from '../assets/img/logo_css.png'
+import brush from '../assets/img/logo_brush.png'
 
 const Home = () => {
   return (
@@ -27,21 +30,21 @@ const Home = () => {
       </div>
 
       {/*Html,Css,Graphic Design  */}
-      
+
       <section class="main-bottom">
         <div>
-            <img src="./img/logo_html.png" alt="html"/>
+            <img src={html} alt="html"/>
             <h3>Html5 Markup</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, est.</p>
         </div>
         <div>
-            <img src="./img/logo_css.png" alt="css"/>
-            <h3>Html5 Markup</h3>
+            <img src={css} alt="css"/>
+            <h3>CSS3 Styling</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, est.</p>
         </div>
         <div>
-            <img src="./img/logo_brush.png" alt="brush"/>
-            <h3>Html5 Markup</h3>
+            <img src={brush} alt="brush"/>
+            <h3>Graphic Design</h3>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, est.</p>
         </div>
     </section>
