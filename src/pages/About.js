c5025f079f6860cc733fa6aa633e8a692642770a
elcinp <elcinpars1@gmail.com>
2021-09-16 11:46:03 +0300

    about.js added

---
+++ src/pages/About.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div class="about-main">
+        <div class="about-main-left">
+          <div class="about-main-left1">
+            <h3>About Us</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Architecto minus hic error tempora aperiam, veritatis eaque quod
+              ducimus fuga cupiditate! Voluptates non ullam, officiis sint
+              molestiae ex, inventore maxime, vero facere ab quod asperiores
+              architecto ad commodi blanditiis molestias. Lorem ipsum dolor sit
+              amet. Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+            </p>
+          </div>
+          <div class="about-main-left2">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+              perspiciatis explicabo nihil atque possimus laborum enim
+              voluptatibus nulla nemo et! Non commodi totam velit sunt nostrum
+              adipisci ut harum quas nisi? Alias vel provident quam voluptatibus
+              corporis obcaecati exercitationem. Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit.{" "}
+            </p>
+          </div>
+        </div>
+
+        <div class="about-main-right">
+          <h3>What We Do</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+            sint libero, animi culpa aspernatur commodi totam a nobis excepturi
+            at iure numquam, quos ullam hic. Tenetur eveniet autem hic id.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
